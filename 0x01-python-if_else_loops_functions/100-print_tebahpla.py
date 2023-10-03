9dfb68c095e7b1e115d8d154f85ea30fe504d911
Nomee07 <gigabanomfundo@gmail.com>
2023-10-03 11:06:21 -0700

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
+#!/usr/bin/python3
+
+for i in range(122, 96, -1):
     if i % 2 == 0:
-        print(f"{chr(i)}{chr(i - 32)}", end='')
+        print("{0}{1}".format(chr(i), chr(i - 32)), end='')
     else:
-        print(f"{chr(i - 32)}{chr(i)}", end='')
+        print("{0}{1}".format(chr(i - 32), chr(i)), end='')
