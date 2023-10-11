0d038614e261a24bf462f610371ba9da0de9d94e
Nomee07 <gigabanomfundo@gmail.com>
2023-10-11 03:59:43 -0700

    Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_integers = set()
+    for num in my_list:
+        if isinstance(num, int):
+            uniq_integers.add(num)
+        sum_uniq_integers = sum(uniq_integers)
+    return sum_uniq_integers
