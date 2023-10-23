22cb33f90fc4b930325e135811d4c18007e33465
Nomee07 <gigabanomfundo@gmail.com>
2023-10-22 23:17:32 -0700

    Integers division with debug

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    try:
+        for i in range(x):
+            try:
+                value = int(my_list[i])
+                print("{:d}".format(value), end="")
+                count += 1
+            except (ValueError, TypeError):
+                pass
+    except IndexError:
+        pass
+    finally:
+        print()
+    return count
