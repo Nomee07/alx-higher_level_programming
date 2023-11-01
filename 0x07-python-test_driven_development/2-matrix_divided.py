cda5720a61e434621cce37856f8c11405e1c6970
Nomee07 <gigabanomfundo@gmail.com>
2023-11-01 00:24:40 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,23 +12,22 @@ def matrix_divided(matrix, div):
         div (int or float): The number to divide all elements by
     """
 
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix "
-                        "(list of lists) of integers/floats")
-
+    if not isinstance(matrix, list)\
+            or not all(isinstance(raw, list) for raw in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(n, (int, float)) for row in matrix for n in row):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    row_lengths = [len(row) for row in matrix]
-    if len(set(row_lengths)) != 1:
-        raise ValueError("Each row of the matrix must have the same size")
-
     new_matrix = []
-    for row in matrix:
-        new_row = [round(x / div, 2) for x in row]
-        new_matrix.append(new_row)
-
+    for raw in matrix:
+        resulted_raw = [round(n/div, 2) for n in raw]
+        new_matrix.append(resulted_raw)
     return new_matrix
