555725bb23521f75863f50b74d43b7ba79af60f6
Nomee07 <gigabanomfundo@gmail.com>
2023-11-06 06:28:53 -0800

    Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,9 +2,12 @@
 
 
 """Write a class Square that inherits from Rectangle"""
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
+    """Represent a square."""
+
+
     def __init__(self, size):
         """
         Initialize a Square instance with a specified size.
@@ -16,11 +19,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        """Return a string representation of the square."""
-        return f"[Square] {self.__size}/{self.__size}"
-
-    def __repr__(self):
-        """Return a string representation of the square."""
-        return self.__str__()
