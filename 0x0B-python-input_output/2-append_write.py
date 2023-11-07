a43b6ce5623bd1faf07e35becfccc834f4960054
Nomee07 <gigabanomfundo@gmail.com>
2023-11-07 02:32:26 -0800

    Input and Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,10 +12,5 @@ def append_write(filename="", text=""):
         text (str): The string to be appended to the file.
 
     """
-    try:
-        with open(filename, "a", encoding="utf-8") as file:
-            num_chars_added = file.write(text)
-        return num_chars_added
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return 0
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
