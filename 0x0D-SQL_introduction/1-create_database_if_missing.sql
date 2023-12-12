40e8dffc626cb7ae37efbcedb464c64a35924552
Nomee07 <gigabanomfundo@gmail.com>
2023-12-12 02:43:24 -0800

    List and create a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,5 @@
+-- 1-create_database_if_not_exists.sql
+-- Script to create the database hbtn_0c_0 if it doesn't already exist.
+
+-- Create the database only if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
