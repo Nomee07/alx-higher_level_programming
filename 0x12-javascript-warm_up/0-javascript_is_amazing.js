d1cfd7eee21dd5554f6e888f88bc340e0daf42ea
Nomee07 <gigabanomfundo@gmail.com>
2024-01-08 02:29:49 -0800

    JavaScript is amazing

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+
+// This is a simple JavaScript script
+// that prints "JavaScript is amazing"
+// using console.log.
+
+const myVar = "JavaScript is amazing";
+console.log(myVar);
